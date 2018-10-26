5556e3099024f1d6f08f76c2243d7f4af747f5de
rgsax <riccardog.sax@gmail.com>
2018-10-26 09:57:20 +0200

    first real commit

---
+++ web computing/src/persistence/dao/CorsoDiLaureaDao.java
@@ -0,0 +1,16 @@
+package persistence.dao;
+
+import java.util.List;
+
+import model.CorsoDiLaurea;
+
+public interface CorsoDiLaureaDao {
+	public void save(CorsoDiLaurea corsoDiLaurea);  // Create
+	public CorsoDiLaurea findByPrimaryKey(Long codice);     // Retrieve
+	public List<CorsoDiLaurea> findAll();
+	
+	public CorsoDiLaurea findByPrimaryKeyJoin(Long id);
+	
+	public void update(CorsoDiLaurea corsoDiLaurea); //Update
+	public void delete(CorsoDiLaurea corsoDiLaurea); //Delete	
+}
